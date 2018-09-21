4f030152273ff9ede0d959e7d8a366992403f6c5
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-09-21 11:23:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,9 @@
 def roll_call_dwarves(array)
-  array = array.sort
-  array.each_with_index {|item, index|}
+  i = 0 
+  while i < array.length 
+    puts "#{i + 1}. #{array[i]}"
+    i += 1 
+  end 
 end
 
 
